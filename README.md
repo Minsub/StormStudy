ca04495440f1373278cba22a14b637ce87259256
jiminsub <jiminsub@gmail.com>
2016-10-20 16:25:44 +0900

    Trident Topology Sample

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # StormStudy
-Apache Storm Study
+Apache Storm을 공부 해 보자!!
+
+## 개발환경 구성
+- Java: 1.8
+- Storm-core: 1.0.2
+- Zookeeper: 3.4.9
+
---
+++ src/main/java/com/minsub/storm/trident/spout/DefaultCoordinator.java
@@ -0,0 +1,36 @@
+package com.minsub.storm.trident.spout;
+
+import org.apache.storm.trident.spout.ITridentSpout;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+
+/**
+ * Created by jiminsub on 2016. 10. 20..
+ */
+public class DefaultCoordinator implements ITridentSpout.BatchCoordinator<Long>, Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultCoordinator.class);
+
+    @Override
+    public Long initializeTransaction(long l, Long aLong, Long x1) {
+        LOG.info("Initializing Transaction [" + l + "]");
+        return null;
+    }
+
+    @Override
+    public void success(long l) {
+        LOG.info("Successful Transaction [" + l + "]");
+    }
+
+    @Override
+    public boolean isReady(long l) {
+        return true;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
